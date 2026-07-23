952912d3902aeca10dede5097eb94ff215ba2d42
Abhinav Tiwari <atiwari@newrelic.com>
2026-07-23 11:59:30 +0530

    feat: [receiver/nrmysql] Scaffold nrmysqlreceiver as a fork of mysqlreceiver

---
+++ receiver/nrmysqlreceiver/doc.go
@@ -0,0 +1,6 @@
+// Copyright The OpenTelemetry Authors
+// SPDX-License-Identifier: Apache-2.0
+
+//go:generate make mdatagen
+
+package nrmysqlreceiver // import "github.com/newrelic-forks/opentelemetry-collector-contrib/receiver/nrmysqlreceiver"
